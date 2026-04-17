dadd9ef97791147231e704720c1f6ed2f1a9418d
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-04-18 01:31:52 +0530

    Cland-format

--- backend/CaseConversionAPI/CppLib/src/RemoveSpacesConversion.cpp
+++ backend/CaseConversionAPI/CppLib/src/RemoveSpacesConversion.cpp
@@ -26,8 +26,7 @@
 #include "RemoveSpacesConversion.hpp"
 #include <algorithm>
 
-std::string 
-RemoveSpacesConversion::convert(const std::string& input) const {
+std::string RemoveSpacesConversion::convert(const std::string& input) const {
     std::string result = input;
     result.erase(std::remove(result.begin(), result.end(), ' '), result.end());
     return result;
