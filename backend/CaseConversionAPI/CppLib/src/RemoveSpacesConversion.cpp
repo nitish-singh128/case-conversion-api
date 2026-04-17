080c88d61d0d10d0fce4daff0516562c86c41e4f
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-04-18 01:27:16 +0530

    clang-formatted code Remove spaces conversion

--- backend/CaseConversionAPI/CppLib/src/RemoveSpacesConversion.cpp
+++ backend/CaseConversionAPI/CppLib/src/RemoveSpacesConversion.cpp
@@ -22,16 +22,12 @@
 /*********************************************************************/
 /* Dependencies                                                      */
 /*********************************************************************/
+
 #include "RemoveSpacesConversion.hpp"
 #include <algorithm>
 
 std::string RemoveSpacesConversion::convert(const std::string& input) const {
     std::string result = input;
-
-    result.erase(
-        std::remove(result.begin(), result.end(), ' '),
-        result.end()
-    );
-
+    result.erase(std::remove(result.begin(), result.end(), ' '), result.end());
     return result;
 }
